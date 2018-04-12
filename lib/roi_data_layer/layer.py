4f49b97aace19b67218fb674a06db6e9e934d762
amands97 <aman150084@gmail.com>
2018-04-12 07:49:56 +0530

    try changes

--- lib/roi_data_layer/layer.py
+++ lib/roi_data_layer/layer.py
@@ -122,7 +122,7 @@ class RoIDataLayer(caffe.Layer):
         """Get blobs and copy them into this layer's top blob vector."""
 
         blobs = self._get_next_minibatch()
-        print(blobs)
+        # print(blobs)
         for blob_name, blob in blobs.iteritems():
             # print("blob", blob)
             top_ind = self._name_to_top_map[blob_name]
