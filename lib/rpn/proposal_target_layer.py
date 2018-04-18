898cacf92bfb95f2565d44b33dbd3e10bce6b746
amands97 <aman150084@gmail.com>
2018-04-18 07:01:45 +0530

    do something

--- lib/rpn/proposal_target_layer.py
+++ lib/rpn/proposal_target_layer.py
@@ -82,6 +82,8 @@ class ProposalTargetLayer(caffe.Layer):
               all_rois, gt_wins, fg_rois_per_image,
               rois_per_image, self._num_classes)
         print("labels we got", labels)
+        print((labels > 0))
+        print("a", (labels > 0).sum())
         print 'num fg: {}'.format((labels > 0).sum())
         print 'num bg: {}'.format((labels == 0).sum())
         if DEBUG:
