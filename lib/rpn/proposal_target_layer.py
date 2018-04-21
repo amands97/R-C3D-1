7984c933cb399383936ea06ba64df76bb7b73944
amands97 <aman150084@gmail.com>
2018-04-21 23:33:59 +0530

    try hard sampling

--- lib/rpn/proposal_target_layer.py
+++ lib/rpn/proposal_target_layer.py
@@ -65,7 +65,7 @@ class ProposalTargetLayer(caffe.Layer):
         assert np.all(all_rois[:, 0] == 0), \
                 'Only single item batches are supported'
 
-
+        self._sample == "All"
         if self._sample == "All":
           labels, rois, twin_targets, twin_inside_weights = _sample_all_rois(
               all_rois, gt_wins, self._num_classes)
